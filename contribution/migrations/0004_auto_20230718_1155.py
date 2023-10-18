d0ab0d89cf8a5e4a167881ce0aef5cba5cbf8387
Dragos DOBRE <dragos.dobre@swisstph.ch>
2023-10-18 15:35:17 +0200

    MERGING RELEASE branches (#48)
    * CI update
    
    * CI update 2
    
    * CI Update 2
    
    * OP-1509: add missing migrations to the module (#45)
    
    * OP-1509: add missing migrations to the module
    
    * OP-1509: add new ci job
    
    * OP-1509: change name fo field in migration
    
    * OP-1509: migrate only if Premium has no source and source version
    
    * OP-1509: refactor to not to duplicate
    
    * fix test
    
    ---------
    
    Co-authored-by: Jan <j.dolkowski@soldevelo.com>
    Co-authored-by: delcroip <patrick.delcroix@swisstph.ch>
    
    ---------
    
    Co-authored-by: Damian Borowiecki <dborowiecki@soldevelo.com>
    Co-authored-by: Jan <jdolkowski@soldevelo.com>
    Co-authored-by: Jan <j.dolkowski@soldevelo.com>
    Co-authored-by: delcroip <patrick.delcroix@swisstph.ch>

--- contribution/migrations/0004_auto_20230718_1155.py
+++ contribution/migrations/0004_auto_20230718_1155.py
@@ -2,20 +2,7 @@
 from django.conf import settings
 from django.db import migrations, models
 
-
-class AddFieldPostgres(migrations.AddField):
-    def database_forwards(self, app_label, schema_editor, from_state, to_state):
-        if schema_editor.connection.vendor == 'postgresql':
-            super().database_forwards(app_label, schema_editor, from_state, to_state)
-
-    def database_backwards(self, app_label, schema_editor, from_state, to_state):
-        if schema_editor.connection.vendor == 'postgresql':
-            super().database_backwards(app_label, schema_editor, from_state, to_state)
-
-    def describe(self):
-        # This is used to describe what the operation does in console output.
-        return "Wrapper for AddField that works only for postgres database engine."
-
+from contribution.utils import AddFieldPostgres
 
 
 class Migration(migrations.Migration):
@@ -23,7 +10,7 @@ class Migration(migrations.Migration):
         ('contribution', '0003_alter_premium_options'),
     ]
 
-    # For MSSQL This changes were added through raw sql, to keep consistency with existing databases it couldn't be
+    # For MSSQL These changes were added through raw sql, to keep consistency with existing databases it couldn't be
     # changed in postgres sql script.
     operations = []
     if not settings.MSSQL:
