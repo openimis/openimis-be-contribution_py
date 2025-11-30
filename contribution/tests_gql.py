ec3f67e23af67b2565a507a21cbb9aae34207184
delcroip <patrick.delcroix@swisstph.ch>
2025-11-30 20:45:39 +0100

    refactor(contribution/tests): update ContributionGQLTestCase setup for better test isolation
    Replace hardcoded role ID and database queries for Payer/Product with dedicated test helpers (create_test_payer, create_test_product, create_clerk_role). Add create_basic_test_locations call to ensure consistent location setup. This improves test reliability by avoiding dependencies on existing data.

--- contribution/tests_gql.py
+++ contribution/tests_gql.py
@@ -3,18 +3,24 @@ import json
 from dataclasses import dataclass
 
 from core.models import User, filter_validity
-from core.test_helpers import create_test_interactive_user
+from core.test_helpers import create_test_interactive_user, create_clerk_role
 from django.conf import settings
 from graphene_django.utils.testing import GraphQLTestCase
 from graphql_jwt.shortcuts import get_token
 from location.models import Location
-from location.test_helpers import create_test_location, assign_user_districts
+from location.test_helpers import (
+  create_test_location,
+  assign_user_districts,
+  create_basic_test_locations,
+  create_test_health_facility,
+  create_test_village
+)
 from rest_framework import status
 from insuree.test_helpers import create_test_insuree
 from policy.test_helpers import create_test_policy
+from product.test_helpers import create_test_product
+from payer.test_helpers import create_test_payer
 from contribution.models import Premium, PayTypeChoices
-
-from location.test_helpers import create_test_location, create_test_health_facility, create_test_village
 from payer.models import Payer
 from product.models import Product
 import datetime
@@ -40,17 +46,19 @@ class ContributionGQLTestCase(openIMISGraphQLTestCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
+        create_basic_test_locations()
         cls.test_village = create_test_village()
         cls.test_insuree = create_test_insuree(with_family=True, is_head=True, custom_props={'current_village':cls.test_village}, family_custom_props={'location':cls.test_village})
         cls.admin_user = create_test_interactive_user(username="testLocationAdmin")
         cls.admin_token = get_token(cls.admin_user, DummyContext(user=cls.admin_user))
-        cls.ca_user = create_test_interactive_user(username="testLocationNoRight", roles=[9])
+        clerk_role = create_clerk_role()
+        cls.ca_user = create_test_interactive_user(username="testLocationNoRight", roles=[clerk_role.id])
         cls.ca_token = get_token(cls.ca_user, DummyContext(user=cls.ca_user))
         cls.admin_dist_user = create_test_interactive_user(username="testLocationDist")
         assign_user_districts(cls.admin_dist_user, ["R1D1", "R2D1", "R2D2", "R2D1", cls.test_village.parent.parent.code])
         cls.admin_dist_token = get_token(cls.admin_dist_user, DummyContext(user=cls.admin_dist_user))
-        cls.payer = Payer.objects.filter(*filter_validity()).first()
-        cls.product = Product.objects.filter(*filter_validity()).first()
+        cls.payer = create_test_payer()
+        cls.product = create_test_product("ELI1")
         cls.policy = create_test_policy(cls.product, cls.test_insuree, custom_props={'value':1000}, link=True, valid=True)
 
 
@@ -222,9 +230,3 @@ class ContributionGQLTestCase(openIMISGraphQLTestCase):
         content = json.loads(response.content)
 
         self.assertResponseNoErrors(response)
-
-        
-
-
-
-
